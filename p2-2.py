aebc18d3fbc02bf6ac8520e5013a058a35361a7f
digital-wossen <wossen.k11@gmail.com>
2019-07-23 09:59:04 +0200

    first commit

---
+++ p2-2.py
@@ -0,0 +1,53 @@
+# Wossen Hailemariam
+class Node:
+    def __init__(self, d, ch):
+        self.data = d
+        self.children = ch
+
+    def __str__(self):
+        return str(self.data)
+
+    def getData(self):
+        return self.data
+
+    def getChildren(self):
+        return self.children
+
+class Tree:
+    def __init__(self,r):
+        self.root = r
+
+    def getRoot(self):
+        return self.root
+
+class IteratorBFS:
+    def __init__(self, t):
+        self.tree = t
+        self.stack = []
+        self.stack.append(self.tree.getRoot())
+
+    def hasNext(self):
+        return not len(self.stack) == 0
+
+    def getNext(self):
+        if not self.hasNext():
+            print("END")
+            return None
+        res = self.stack.pop(0)
+        childs = res.getChildren()
+        for c in childs:
+            self.stack.append(c)
+        return res
+
+leaves = []
+for i in range(10):
+    leaves.append(Node(i,[]))
+nodes = []
+for i in range(5):
+    nodes.append(Node(i+10, [leaves[2*i], leaves[2*i+1]]))
+nodes1 = [Node(15,[nodes[0], nodes[1], nodes[2]]), Node(16,[nodes[3], nodes[4]])]
+root = Node(17,nodes1)
+tree = Tree(root)
+it = IteratorBFS(tree)
+while it.hasNext():
+    print(it.getNext())
